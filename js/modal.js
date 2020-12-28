f66058d40374c3b63c293def9bcecc4093cb3ca4
Bulat <57670154+MadPuffer@users.noreply.github.com>
2020-12-28 15:59:09 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
-var modal = document.getElementById("myModal");
-var btn = document.querySelectorAll(".buy");
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("myModal");
+let btn = document.querySelectorAll(".buy");
+let span = document.getElementsByClassName("close")[0];
 
 btn.forEach(function(item){
     item.onclick = function() {
@@ -16,4 +16,19 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+function sendBtn() {
+    let name = document.querySelector(".modal_name");
+    let email = document.querySelector(".modal_email");
+    let msg = document.querySelector(".modal_msg");
+    let dataToSend = {"name": name, "email": email, "msg": msg};
+    $.ajax({
+        url : "/sendBtn",
+        type : "POST",
+        data : JSON.stringify(dataToSend),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+
+    })
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     const screen_width = screen.width;
+    if (screen_width > 1279){
     let main = document.querySelectorAll(".nav-iem");
     let summary = 0;
     main.forEach(function(item){
@@ -7,8 +8,61 @@ document.addEventListener("DOMContentLoaded", function(){
                  });
     main.forEach(function(item){
                  item.style.marginLeft = (screen_width - summary) / 7 + "px";
-                 });
-    
+
+    });
+    }
+
+    else if ( screen_width <= 320){
+        let main = document.querySelectorAll(".nav-iem");
+        let summary = 0;
+        main.forEach(function(item){
+            item.style.marginLeft = screen_width * 1.2 / 7 + "px";
+
+        });
+    }
+
+    else if (320 < screen_width <= 425){
+        let main = document.querySelectorAll(".nav-iem");
+        let summary = 0;
+        main.forEach(function(item){
+            item.style.marginLeft = screen_width  / 7 + "px";
+
+        });
+    }
+
+    else if (425 < screen_width < 769){
+        let main = document.querySelectorAll(".nav-iem");
+        let summary = 0;
+        main.forEach(function(item){
+            item.style.marginLeft = screen_width * 0.5 / 7 + "px";
+
+        });
+    }
+
+    else if (1023 < screen_width <= 1280){
+        let main = document.querySelectorAll(".nav-iem");
+        let summary = 0;
+        main.forEach(function(item){
+            item.style.marginLeft = screen_width * 0.3 / 7 + "px";
+
+        });
+    }
+    if (screen_width === 768){
+        let main = document.querySelectorAll(".nav-iem");
+        main.forEach(function(item){
+            item.style.marginLeft = screen_width * 0.5 / 7 + "px";
+
+        });
+    }
+    if (screen_width === 1024){
+        let main = document.querySelectorAll(".nav-iem");
+        main.forEach(function(item){
+            item.style.marginLeft = screen_width * 0.4 / 7 + "px";
+
+        });
+    }
+
+
 });
 
 $(document).ready(function(){
@@ -16,13 +70,13 @@ $(document).ready(function(){
 });
 
 $('.to-down-btn').on('click', function(){
-  var target = $(this).data('target');
-  var pos = $(target).offset().top;
+    let target = $(this).data('target');
+    let pos = $(target).offset().top;
   $('html, body').animate({'scrollTop': pos - screen.height * 0.09}, 400);
 });
 
 $('.nav-link').on('click', function(){
-  var target = $(this).data('target');
-  var pos = $(target).offset().top;
+    let target = $(this).data('target');
+    let pos = $(target).offset().top;
   $('html, body').animate({'scrollTop': pos - screen.height * 0.09}, 400);
 });
